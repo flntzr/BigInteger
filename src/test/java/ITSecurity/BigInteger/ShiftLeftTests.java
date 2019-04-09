37247f33db76bc40cdc19e51a527a4f7f68ba88d
Franz <fflintzer@gmail.com>
2019-04-09 21:45:50 +0200

    Cut some unused code

--- src/test/java/ITSecurity/BigInteger/ShiftLeftTests.java
+++ src/test/java/ITSecurity/BigInteger/ShiftLeftTests.java
@@ -10,24 +10,13 @@ import junit.framework.TestSuite;
 public class ShiftLeftTests extends TestCase {
 
 	/**
-	 * Create the test case
-	 *
-	 * @param testName name of the test case
-	 */
-	public ShiftLeftTests(String testName) {
-		super(testName);
-	}
-
-	/**
 	 * @return the suite of tests being tested
 	 */
 	public static Test suite() {
 		return new TestSuite(ShiftLeftTests.class);
 	}
 
-//	@Test
 	public void test() {
-		System.out.println(new File(".").getAbsolutePath());
 		TestFileReader reader = new TestFileReader("src/test/java/ITSecurity/BigInteger/test_files/Shift-Left-Tests.txt");
 		List<ImportedTest> tests = reader.read();
 	}
